e1e7692361935911d4aee18b0cddd1220b518cf9
ZarinaMambetova <zarina.mambetova84@gmail.com>
2020-09-14 03:17:52 +0300

    решена проблема с px2rem

--- gulpfile.js
+++ gulpfile.js
@@ -7,142 +7,147 @@ const browserSync = require('browser-sync').create(); // Подключили п
 const reload = browserSync.reload; // Сам перезагружает сервер, если есть изменения
 
 const sassGlob = require('gulp-sass-glob'); // Подключили пакет: упрощение подключения всех файлов стилей одной строкой @import "./layout/*.scss";. Установка: npm install gulp-sass-glob --save-dev
-const  autoprefixer = require('gulp-autoprefixer'); // Подключаем пакет autoprefixer для свойств под разные браузеры. Установка: npm install --save-dev gulp-autoprefixer
+const autoprefixer = require('gulp-autoprefixer'); // Подключаем пакет autoprefixer для свойств под разные браузеры. Установка: npm install --save-dev gulp-autoprefixer
 const cleanCSS = require('gulp-clean-css'); // Подключили пакет: для минимизации файла стилей. Установка:  npm install gulp-clean-css --save-dev
 const sourcemaps = require('gulp-sourcemaps'); // Подключили пакет: для карты файлов в панели разработчика. Установка: npm i gulp-sourcemaps --save-dev
 
-const px2rem = require('gulp-px-to-rem'); // Переводит в ремы пиксели для адаптива. Установка:  npm i --save-dev  gulp-smile-px2rem
+const px2rem = require('gulp-smile-px2rem'); // Переводит в ремы пиксели для адаптива. Установка:  npm i --save-dev  gulp-smile-px2rem
 const gcmq = require('gulp-group-css-media-queries'); // Подключили пакет: группировка медиа-запросов для минимизации файла стилей. Установка: npm install --save-dev gulp-group-css-media-queries
 
 const babel = require('gulp-babel'); // Подключили пакет: преобразовываем js-код для старых браузеров. Установка:  npm i gulp-sourcemaps --save-dev
 const uglify = require('gulp-uglify'); // Подключили пакет: для минимизации файла со скриптами. Установка:  npm install --save-dev gulp-uglify
 
-const svgo = require('gulp-svgo'); // Подключили пакет: оптимизация XML-кода в SVG 
+const svgo = require('gulp-svgo'); // Подключили пакет: оптимизация XML-кода в SVG
 const svgSprite = require('gulp-svg-sprite'); // Подключили пакет: создание спрайтов SVG. Установка с предыдущим пакетом: npm i gulp-svgo gulp-svg-sprite --save-dev
 
-const  gulpif = require('gulp-if');// Подключили пакет: разделения, когда какие пакеты должны срабатывать (dev & prod). Установка: npm i gulp-if --save-dev
+const gulpif = require('gulp-if'); // Подключили пакет: разделения, когда какие пакеты должны срабатывать (dev & prod). Установка: npm i gulp-if --save-dev
 const env = process.env.NODE_ENV; // установили cross-env для корректной работы во всех системах (ос, линуkc, вин). Установка: npm i cross-env  --save-dev Внесли изменения в файл package.json
 
-const {DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS} = require('./gulp.config'); // подключили файл gulp.config.js
+const { DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS } = require('./gulp.config'); // подключили файл gulp.config.js
 
 sass.compiler = require('node-sass'); // обязательно указываем компилятор на ноде
 
 // npm run gulp clean - Удаление папки dist и всего ее содержимого, если меняются исходники. При пересборке проекта
-task( 'clean', () => {
-  return src( `${DIST_PATH}/**/*`, { read: false }).pipe( rm() )
+task('clean', () => {
+  return src(`${DIST_PATH}/**/*`, { read: false }).pipe(rm());
 });
 
 // npm run gulp copy - Копируем содержимое с расширением .html в папку dist
 // После копирования перезагружается сервер внутри потока
-task( 'copy:html', () => {
-  return src( `${SRC_PATH}/*.html` )
-  .pipe(dest(DIST_PATH))
-  .pipe(reload({stream: true}));
-
+task('copy:html', () => {
+  return src(`${SRC_PATH}/*.html`).pipe(dest(DIST_PATH)).pipe(reload({ stream: true }));
 });
 
 // Копируем фото
-task( 'copy:images', () => {
-  return src( `${SRC_PATH}/images/pic/**/*.*`)
-  .pipe(dest(`${DIST_PATH}/images/pic`))
-  .pipe(reload({stream: true}));
-
+task('copy:images', () => {
+  return src(`${SRC_PATH}/images/pic/**/*.*`).pipe(dest(`${DIST_PATH}/images/pic`)).pipe(reload({ stream: true }));
 });
 
-
 // Копируем шрифты
-task( 'copy:fonts', () => {
-  return src( `${SRC_PATH}/fonts/*.*`)
-  .pipe(dest(`${DIST_PATH}/fonts`))
-  .pipe(reload({stream: true}));
-
+task('copy:fonts', () => {
+  return src(`${SRC_PATH}/fonts/*.*`).pipe(dest(`${DIST_PATH}/fonts`)).pipe(reload({ stream: true }));
 });
 
 // Далее: npm run gulp styles - Запускает таск по обработке файлов стилей. Следим за  файлами стилей,
 // gulpif указывает, какой пакет должен сработать только для стадии разработки, а какой на продакшн
 // 1 Инициализация карты файлов
 // 2 Склеиваем в один файл содержимое массива
-// 3 sassGlob собирает все содержимое папки layout в main  
+// 3 sassGlob собирает все содержимое папки layout в main
 // 4 Обработка sass-ом. Добавили предупреждение об ошибке
-// 5 Переводит в ремы пиксели 
+// 5 Переводит в ремы пиксели
 // 6 Автопрефиксы работают с css-файлами, поэтому подключаем после компиляции
 // 7  группировка медиа-запросов (после компиляции)
 // 8 минимизируем файл (пробелы, комменты и пр.). Файл весит меньше
 // 9 запись сорсмапов
 // 10 собираем всё в dist
 // 11 Перезагрузка сервера
-task( 'styles', () => {
-  return src([...STYLES_LIBS, `${SRC_PATH}/styles/main.scss`])
-    .pipe(gulpif(env === "dev", sourcemaps.init()))
-    .pipe(concat('main.min.scss'))
-    .pipe(sassGlob())
-    .pipe(sass().on('error', sass.logError))
-    // .pipe(px2rem()) неправильно считает, уменьшает значения. Исправить!
-    .pipe(gulpif(env === "dev", autoprefixer({cascade: false})))
-    .pipe(gulpif(env === "prod", gcmq()))
-    .pipe(gulpif(env === "prod", cleanCSS({compatibility: 'ie8'})))
-    .pipe(gulpif(env === "dev", sourcemaps.write()))
-    .pipe(dest(DIST_PATH))
-    .pipe(reload({stream: true}));
+task('styles', () => {
+  return (
+    src([ ...STYLES_LIBS, `${SRC_PATH}/styles/main.scss` ])
+      .pipe(gulpif(env === 'dev', sourcemaps.init()))
+      .pipe(concat('main.min.scss'))
+      .pipe(sassGlob())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(px2rem({
+        dpr: 1,             // base device pixel ratio (default: 2)
+        rem: 16,            // root element (html) font-size (default: 16)
+        one: false          // whether convert 1px to rem (default: false)
+      })) 
+      .pipe(gulpif(env === 'dev', autoprefixer({ cascade: false })))
+      .pipe(gulpif(env === 'prod', gcmq()))
+      .pipe(gulpif(env === 'prod', cleanCSS({ compatibility: 'ie8' })))
+      .pipe(gulpif(env === 'dev', sourcemaps.write()))
+      .pipe(dest(DIST_PATH))
+      .pipe(reload({ stream: true }))
+  );
 });
 
 // Далее: npm run gulp scripts - Запускает таск по обработке файлов js.
 // 1 Инициализация карты файлов
-// 2 Склеиваем в один файл 
+// 2 Склеиваем в один файл
 // 3преобразовываем синтаксис в понятный всем браузерам
 // 4 Минимизируем код
 // 5 запись сорсмапов
 // 6 собираем всё в dist
 // 7 Перезагрузка сервера
-task( 'scripts', () => {
-  return src([...JS_LIBS, `${SRC_PATH}/scripts/*.js`])
-    .pipe(gulpif(env === "dev", sourcemaps.init()))
-    .pipe(concat('main.min.js', { newLine: ";"}))
-    .pipe(gulpif(env === "dev", babel({
-      presets: ['@babel/env']
-  })))
-    .pipe(gulpif(env === "dev", uglify()))
+task('scripts', () => {
+  return src([ ...JS_LIBS, `${SRC_PATH}/scripts/*.js` ])
+    .pipe(gulpif(env === 'dev', sourcemaps.init()))
+    .pipe(concat('main.min.js', { newLine: ';' }))
+    .pipe(
+      gulpif(
+        env === 'dev',
+        babel({
+          presets: [ '@babel/env' ]
+        })
+      )
+    )
+    .pipe(gulpif(env === 'dev', uglify()))
     .pipe(sourcemaps.write())
     .pipe(dest(DIST_PATH))
-    .pipe(reload({stream: true}));
+    .pipe(reload({ stream: true }));
 });
 
 // Работа с SVG:
-// 1 убираем все лишние атрибуты в файлах 
+// 1 убираем все лишние атрибуты в файлах
 // 2 собираем спрайт
 // 3 отправляем в папку dist
-task( 'icons', () => {
-  return src( `${SRC_PATH}/images/icons/*.svg` )
-  .pipe(svgo({
-    plugins: [
-      {
-        removeAttrs: { attrs: "(style|data.*)"}
-      }
-    ]
-  }))
-    .pipe(svgSprite({
-      mode: {
-        symbol: {
-          sprite: "../sprite.svg"
+task('icons', () => {
+  return src(`${SRC_PATH}/images/icons/*.svg`)
+    .pipe(
+      svgo({
+        plugins: [
+          {
+            removeAttrs: { attrs: '(style|data.*)' }
+          }
+        ]
+      })
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.svg'
+          }
         }
-      }
-    }))
-    .pipe(dest(`${DIST_PATH}/images/icons`))
-  });
+      })
+    )
+    .pipe(dest(`${DIST_PATH}/images/icons`));
+});
 
-// Развернем dev-server по папке dist. 
+// Развернем dev-server по папке dist.
 // open: false - Не открывать каждый раз новую вкладку при запуске
 task('server', () => {
   browserSync.init({
-      server: {
-          baseDir: `./${DIST_PATH}`
-      },
-      open: false
+    server: {
+      baseDir: `./${DIST_PATH}`
+    },
+    open: false
   });
 });
 
 // следим за изменеиями в файлах методом watch
-task("watch", () => {
+task('watch', () => {
   watch('./src/styles/**/*.scss', series('styles'));
   watch('./src/*.html', series('copy:html'));
   watch('./src/scripts/*.js', series('scripts'));
@@ -150,22 +155,16 @@ task("watch", () => {
   watch('./src/images/pic/**/*.*', series('copy:images'));
 });
 
-
-
 // npm run gulp - указываем выше все таски и одной командой запускаем серию тасков в терминале
 // Указываем, какие таски должны сработать параллельно
 task(
-  'default', 
+  'default',
   series(
-    'clean', 
-    parallel('copy:html', 'copy:images', 'copy:fonts', 'styles', 'scripts', 'icons'), 
+    'clean',
+    parallel('copy:html', 'copy:images', 'copy:fonts', 'styles', 'scripts', 'icons'),
     parallel('watch', 'server')
-    )
-  );
+  )
+);
 
-  //npm run build - собираем готовый оптимизированный, минимизированный  проект
-  task(
-    'build', 
-    series('clean', 
-      parallel('copy:html', 'copy:images', 'copy:fonts', 'styles', 'scripts', 'icons'))
-    );
+//npm run build - собираем готовый оптимизированный, минимизированный  проект
+task('build', series('clean', parallel('copy:html', 'copy:images', 'copy:fonts', 'styles', 'scripts', 'icons')));
